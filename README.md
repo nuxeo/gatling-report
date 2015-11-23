b2ca7b9851ba3258dc5ba106b8de59172cb23379
Benoit Delbosc <bdelbosc@nuxeo.com>
2015-11-23 17:35:05 +0100

    init capsule empty app

---
+++ README.md
@@ -0,0 +1,35 @@
+# Load Gatling simulation.log to create report
+
+# Install
+
+## Download
+
+## Building from sources
+
+Create the all in one jar:
+
+        mvn package
+
+The jar is located here:
+
+        ./target/gatling-report-VERSION-capsule-full.jar
+
+# Usage
+
+## Configuration
+
+## Invocation
+
+# About Nuxeo
+
+Nuxeo provides a modular, extensible Java-based
+[open source software platform for enterprise content management](http://www.nuxeo.com/en/products/ep)
+and packaged applications for
+[document management](http://www.nuxeo.com/en/products/document-management),
+[digital asset management](http://www.nuxeo.com/en/products/dam) and
+[case management](http://www.nuxeo.com/en/products/case-management). Designed
+by developers for developers, the Nuxeo platform offers a modern
+architecture, a powerful plug-in model and extensive packaging
+capabilities for building content applications.
+
+More information on: <http://www.nuxeo.com/>
---
+++ src/main/java/App.java
@@ -0,0 +1,32 @@
+/*
+ * (C) Copyright 2015 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Benoit Delbosc
+ */
+import org.apache.log4j.Logger;
+
+public class App implements Runnable {
+    private final static Logger log = Logger.getLogger(App.class);
+
+    @Override
+    public void run() {
+        log.info("Starting app...");
+        log.info("End of app");
+    }
+
+
+    public static void main(String args[]) {
+        (new Thread(new App())).start();
+    }
+}
